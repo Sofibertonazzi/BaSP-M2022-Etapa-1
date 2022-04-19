00bbe1c3d03bf114efb77477170be8c031984874
Sofia Bertonazzi <sofi.bertonazzi@gmail.com>
2022-04-19 18:05:48 -0300

    BaSP-W05-2022: Exercise added 01 of variables

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,21 @@
+//exercise A
+
+var number1 = 10;
+var number2 = 15;
+var result = number1 + number2;
+
+console.log(`The string result is: ${result}`);
+
+//exercise B
+
+var color1 = "red";
+var color2 = "blue";
+result = color1 + color2;
+
+//exercise C
+
+var object1 = "chair";
+var object2 = "table";
+result = object1.length + object2.length;
+
+console.log(`The string result is: ${result}`);
